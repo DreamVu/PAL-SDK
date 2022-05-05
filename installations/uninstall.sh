5eab6baf756ff09b797e0c258cf3924ba0dc268c
piyush-312 <40489548+piyush-312@users.noreply.github.com>
2022-05-05 18:15:09 +0530

    Update uninstall.sh

--- installations/uninstall.sh
+++ installations/uninstall.sh
@@ -3,5 +3,10 @@ sed -i '/PAL-Mini-Firmware/ s/^/#/' ~/.bashrc
 sed -i '/PAL-Firmware/ s/^/#/' ~/.bashrc
 sed -i '/PAL-Mini/ s/^/#/' ~/.bashrc
 sed -i '/PAL-ODOA/ s/^/#/' ~/.bashrc
+sed -i '/3D-Object-Detection-Tracking/ s/^/#/' ~/.bashrc
+sed -i '/Obstacle-Detection/ s/^/#/' ~/.bashrc
+sed -i '/PAL-USB s/^/#/' ~/.bashrc
+sed -i '/SLAM s/^/#/' ~/.bashrc
+
 echo "Uninstalled previous versions of Firmwares from bashrc"
 
