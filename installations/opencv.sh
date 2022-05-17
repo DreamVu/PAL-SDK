85c60f888961ea4677912fb6b8865eb034049a6b
Debashish Roy <debashish.roy1998@gmail.com>
2022-05-17 17:18:19 +0530

    git-lfs

--- installations/opencv.sh
+++ installations/opencv.sh
@@ -1,12 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install -y libjasper-dev, python3.6-dev, python3.6-venv, libglew2.0
 
-cd ..
-sudo apt-get install -y git-lfs
-git lfs pull
-cd ./installation
-
-
 sudo mkdir -p /usr/local/lib/pkgconfig/
 sudo cp opencv.pc /usr/local/lib/pkgconfig/
 
