74b1de1b322ca9bace5f2e65202c3935228daa59
Debashish Roy <debashish.roy1998@gmail.com>
2022-05-18 14:37:03 +0530

    remove installation inside script

--- installations/opencv.sh
+++ installations/opencv.sh
@@ -1,6 +1,3 @@
-sudo apt-get update -y
-sudo apt-get install -y libjasper-dev, python3.6-dev, python3.6-venv, libglew2.0
-
 sudo mkdir -p /usr/local/lib/pkgconfig/
 sudo cp opencv.pc /usr/local/lib/pkgconfig/
 
--- installations/setup_python_env.sh
+++ installations/setup_python_env.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
-#sudo rm -rf dreamvu_ws
 
 python3 -m venv dreamvu_ws
 source ./dreamvu_ws/bin/activate
 pip install --upgrade setuptools pip
 
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-#pip install -U grpcio absl-py 
-
-#torch installation
-#wget https://nvidia.box.com/shared/static/wa34qwrwtk9njtyarwt5nvo6imenfy26.whl -O torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 
 pip install Cython
 pip install numpy==1.19.3
 pip install pyyaml imageio opencv-python-headless 
-#pip install torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 
-#torchvision installation
-# git clone --branch v0.8.1 https://github.com/pytorch/vision torchvision   
-# cd torchvision
-# export BUILD_VERSION=0.8.1
-# python3 setup.py install     
-# cd ../ 
-# pip install geffnet
 pip install pyparsing==3.0.8
 
 echo "export LD_LIBRARY_PATH=$"LD_LIBRARY_PATH":`pwd`/../lib" >> ~/.bashrc 
