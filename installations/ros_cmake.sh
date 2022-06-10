fc83923197f290b88594d7c87f3e766dc29dd998
Debashish Roy <debashish.roy1998@gmail.com>
2022-06-10 11:08:20 +0530

    removed installations folder

--- installations/ros_cmake.sh
+++
@@ -1,4 +0,0 @@
-cat /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/cmake_template/header.txt > /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/CMakeLists.txt
-echo "set(PAL_INCLUDE_DIR" /home/$SUDO_USER/DreamVu/PAL-USB/include ")" >> /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/CMakeLists.txt
-echo "set(PAL_LIBRARY" /home/$SUDO_USER/DreamVu/PAL-USB/lib/libPAL_PAL.so  /home/$SUDO_USER/DreamVu/PAL-USB/lib/libPAL_CAMERA_PAL.so /home/$SUDO_USER/DreamVu/PAL-USB/lib/libPAL_Track_PAL.so ")" >> /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/CMakeLists.txt
-cat /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/cmake_template/footer.txt >> /home/$SUDO_USER/DreamVu/PAL-USB/dreamvu_pal_camera/CMakeLists.txt
--- installations/timeout_patch.sh
+++
@@ -1,4 +0,0 @@
-chmod +x rc.local
-sudo cp rc.local /etc/rc.local
-sudo chmod +x /etc/rc.local
-sudo chown -R $SUDO_USER:$SUDO_USER /etc/rc.local
