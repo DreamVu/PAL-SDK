c5ad21153b6cdf753b037c9f5562b56a6efd6970
piyush-312 <40489548+piyush-312@users.noreply.github.com>
2022-07-04 15:01:23 +0530

    Update autostart.sh

--- code_samples/autostart.sh
+++ code_samples/autostart.sh
@@ -2,4 +2,5 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/vpi1/lib64/
 source /home/$USER/DreamVu/dreamvu_ws/bin/activate
+cd /home/$USER/DreamVu/PAL-USB/code_samples
 ./013_object_tracking_GPIO.out
