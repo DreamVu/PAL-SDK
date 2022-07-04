b5d5dcf312f3f2e06b22b004aeb18e50394e0b04
debashish05 <debashish.roy1998@gmail.com>
2022-07-04 14:56:00 +0530

    append path to LD_LiBrary_path

--- code_samples/autostart.sh
+++ code_samples/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$"LD_LIBRARY_PATH":/usr/local/lib
-export LD_LIBRARY_PATH=$"LD_LIBRARY_PATH":/opt/nvidia/vpi1/lib64/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/vpi1/lib64/
 source /home/$USER/DreamVu/dreamvu_ws/bin/activate
 ./013_object_tracking_GPIO.out
