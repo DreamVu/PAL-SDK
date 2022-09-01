d101c8281c6a22b5dadcf2930674f4de8e6be311
piyush <you@example.com>
2022-09-01 17:43:35 +0530

    removed ros pkg and updated ros_demo script

--- ros_demo.sh
+++ ros_demo.sh
@@ -1,6 +1,14 @@
-cp -r ./dreamvu_pal_navigation/ ~/catkin_ws/src/
-cd ~/catkin_ws/
-source ~/catkin_ws/devel/setup.bash
-catkin_make
-source ~/catkin_ws/devel/setup.bash
-roslaunch dreamvu_pal_navigation scan_rviz.launch
+if [ -e /opt/ros/foxy/ ] 
+then
+	cd ~/catkin_ws/
+	colcon build
+	. ./install/setup.bash
+	colcon build
+	ros2 launch src/dreamvu_pal_navigation/launch/scan_rviz_launch.py
+else
+	cd ~/catkin_ws/
+	source ~/catkin_ws/devel/setup.bash
+	catkin_make
+	source ~/catkin_ws/devel/setup.bash
+	roslaunch dreamvu_pal_navigation scan_rviz.launch
+fi
